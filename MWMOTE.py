929498f7e0b92236bbb01e1756c1a2ce4376553e
Wajira Abeysinghe <wpriyadarshani823@gmail.com>
2018-06-05 15:03:23 -0400

    Add files via upload

--- MWMOTE.py
+++ MWMOTE.py
@@ -65,8 +65,6 @@ class Set(object):
                         count_majority = count_majority+1
 
 
-        print count_majority
-
         #if all the 8 neghbours are majority remove, else add
 
         # here we are only adding the sminf
@@ -95,8 +93,7 @@ class Set(object):
                         count_majority = count_majority+1
 
 
-        print count_majority
-
+       
         #if all the 8 neghbours are majority remove, else add
 
         # consider count of majority, if it is  <= 3
@@ -204,17 +201,116 @@ class Set(object):
         return result
 
     def getSets(self):
-        return self.majority_pixel_loc, self.minority_pixel_loc, self.sminf, self.sbmaj, self.simin, self.N_min
+        return self.majority_pixel_loc, self.minority_pixel_loc, self.sminf, self.sbmaj, self.simin, self.N_min, self.org_image_map
 
 
-# class Weight(object):
-#     def __init__(self):
+class Weight(object):
+    def __init__(self, N_min, sbmaj, simin, org_image_map):
+        self.N_min = N_min
+        self.sbmaj = sbmaj
+        self.simin = simin
+        self.org_image_map = org_image_map
+        self.c_max = 2.0
+        self.c_th = 5.0
+       
+        self.I_w = {}
+        self.S_w = {}
+        self.S_p = {}
+
+
+    def generateWeights(self):
+
+        closeness_factor_dic = {}
+
+        for y in self.sbmaj:
+            sum_C_f = 0.
+            for x in self.simin:
+              # closeness_factor
+              #check given sbmaj is in the N_min
+                if y in self.N_min:
+                    if x not in  self.N_min[y]:
+                        closeness_factor = 0.
+                    else:
+                        #calculate the normalized eucledian distance
+                        distance = self.calcEucledianDistance(x, y)
+                        normalized_distance =  distance/255.0
+
+                        if normalized_distance != 0:
+                            #min - closeness factor lied between the [0, self.c_max]
+                            closeness_factor = min(self.c_th, (1 / normalized_distance)) / self.c_th * self.c_max
+                            closeness_factor_dic[(y,x)] = closeness_factor
+                            sum_C_f +=closeness_factor
+                        else:
+
+                            #if pixel values are equal remove the normalized distance and calculate rest
+
+                            closeness_factor = min(self.c_th, (1 )) / self.c_th * self.c_max
+                            closeness_factor_dic[(y,x)] = closeness_factor
+                            sum_C_f +=closeness_factor
+                            # print closeness_factor
+
+        ############dense factor calcultion############################
+
+
+
+            for x in self.simin:
+                key = (y,x)
+                if key in closeness_factor_dic:
+                    closeness_factor = closeness_factor_dic[(y, x)]
+                    densityFactor = closeness_factor/ sum_C_f
+
+                    #weight =  closenes factor * density factor
+                    d = densityFactor * closeness_factor
+
+                    #add the weight
+                    self.I_w[(y,x)] = d
+
+    def generateSWSP(self):
+
+        for x in self.simin:
+            sw = 0.0
+            for y in self.sbmaj:
+                key = (y,x)
+                if key in self.I_w:
+                    sw+=self.I_w[(y, x)]
+                    
+            self.S_w[x] = sw
+
         
-        
+        WeightSum = math.fsum(self.S_w.values())
+        for x in self.S_w:
+            self.S_p[x] = float(self.S_w[x])/WeightSum
+
+    def calcEucledianDistance(self, min_loc, maj_loc):
+        #get the pixel value from the org_image_map
+        maj_pixel = self.org_image_map[maj_loc[0], maj_loc[1]]
+        min_pixel = self.org_image_map[min_loc[0], min_loc[1]]
+
+        dis1 = pow((maj_pixel[0]-min_pixel[0]),2.0)
+        dis2 = pow((maj_pixel[1]-min_pixel[1]),2.0)
+        dis3 = pow((maj_pixel[2]-min_pixel[2]),2.0)
+        dis4 = pow((maj_pixel[3]-min_pixel[3]),2.0)
+
+        sumation = dis1 + dis2 + dis3 + dis4
+        result = numpy.sqrt(sumation)
+        return result
+
+    def getSets(self):
+        return self.I_w, self.S_w, self.S_p
 
 
 if __name__ == "__main__":
     s= Set("T3.png", "GT_T3.png")
     s.openImages()
     s.seperate_classes()
-    majority, minority, sminf, sbmaj, simin, N_min = s.getSets()
+    majority, minority, sminf, sbmaj, simin, N_min, org_image_map = s.getSets()
+
+    print "end of finding sets"
+
+    w = Weight(N_min, sbmaj, simin, org_image_map)
+    w.generateWeights()
+    w.generateSWSP()
+    I_w, S_w, S_p = w.getSets()
+
+    print "end of finding the weights"
+    print "len of weights ", len(I_w)
