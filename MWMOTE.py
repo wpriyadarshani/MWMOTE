8cc32a8c75e644c8312eda3a99f26be0d26f4eeb
Wajira Abeysinghe <wpriyadarshani823@gmail.com>
2018-06-12 11:32:56 -0400

    Add files via upload

--- MWMOTE.py
+++ MWMOTE.py
@@ -9,14 +9,270 @@ from PIL import Image
 import array
 import logging
 
-from deap import algorithms
-from deap import base
-from deap import benchmarks
-from deap import creator
-from deap import tools
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import math
+
+import math, bisect
+
+
+
+class Cluster(object):
+    # Constructor for cluster object
+    def __init__(self):
+        self.pixels = []  # intialize pixels into a list
+        self.centroid = None  # set the number of centro
+        # ids to none
+
+    def addPoint(self, pixel):  # add pixels to the pixel list
+        self.pixels.append(pixel)
+
+
+class fcm(object):
+    # __inti__ is the constructor and self refers to the current object.
+    def __init__(self, oversampled_array, k=3, max_PCA_iterations=10, min_distance=5.0, size=200, m=2.5, epsilon=.5, max_FCM_iterations=100):
+        self.k = k  # initialize k clusters
+
+        # intialize max_iterations
+        self.max_PCA_iterations = max_PCA_iterations
+        self.max_FCM_iterations = max_FCM_iterations
+
+        self.min_distance = min_distance  # intialize min_distance
+        self.degree_of_membership = []
+        self.s = size ** 2
+        self.size = (size, size)  # intialize the size
+        self.m = m
+        self.epsilon = 0.01
+        self.max_diff = 10.0
+        self.image = 0
+        self.pixels2 = []
+        self.pixels = []
+        self.s_new = self.s 
+
+        self.oversampled_array = oversampled_array
+
+    # Takes in an image and performs FCM Clustering.
+    def run(self, image):
+        self.image = image
+        self.image.thumbnail(self.size)
+        self.pixels2 = numpy.array(image.getdata(), dtype=numpy.uint8)
+        # self.beta = self.calculate_beta(self.image)
+
+
+        for i in range(self.s):
+            self.pixels.append(self.pixels2[i])
+
+        for i in range(1):
+            for i in range(len(self.oversampled_array)):
+                self.pixels.append(numpy.asarray(self.oversampled_array[i]))
+
+
+        # set the size
+        self.s_new = self.s + ( 1 *len(self.oversampled_array))
+          
+        print len(self.pixels), len(self.pixels2)
+        print "********** smote array size  ", len(self.oversampled_array)
+
+        self.clusters = [None for i in range(self.k)]
+        self.oldClusters = None
+
+        for i in range(self.s_new):
+            self.degree_of_membership.append(numpy.random.dirichlet(numpy.ones(self.k), size=1))
+
+        for i in range(self.s_new):
+            num_1 = random.randint(1, 2) * 0.1
+            num_2 = random.randint(1, 2) * 0.1
+           
+            num_3 = 1.0 - (num_1+num_2)
+            degreelist = [num_1, num_2, num_3]
+            self.degree_of_membership[i] = degreelist
+
+        randomPixels = random.sample(self.pixels, self.k)
+
+        for idx in range(self.k):
+            self.clusters[idx] = Cluster()
+            self.clusters[idx].centroid = randomPixels[idx]
+            # if(i ==0):
+        for cluster in self.clusters:
+            for pixel in self.pixels:
+                cluster.addPoint(pixel)
+
+        print "________", self.clusters[0].pixels[0]
+        iterations = 0
+
+        # FCM
+        while self.shouldExitFCM(iterations) is False:
+            self.oldClusters = [cluster.centroid for cluster in self.clusters]
+            print "HELLO I A AM ITERATIONS:", iterations
+            print"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            self.calculate_centre_vector()
+
+            self.update_degree_of_membershipFCM()
+           
+
+            iterations += 1
+
+        iterations = 0
+        self.showClustering("FCM.png")
+        # self.DB_index()
+
+        # PCA
+        while self.shouldExitPCA(iterations) is False:
+            self.oldClusters = [cluster.centroid for cluster in self.clusters]
+            print "HELLO I A AM ITERATIONS:", iterations
+            print"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            self.calculate_centre_vector()
+
+            self.update_degree_of_membershipPCA()
+            
+            iterations += 1
+
+        self.showClustering("PCA.png") 
+
+        for cluster in self.clusters:
+            print cluster.centroid
+        return [cluster.centroid for cluster in self.clusters]
+
+
+    def selectSingleSolution(self):
+        self.max_PCA_iterations = 10
+        self.max_FCM_iterations=5
+
+
+    def shouldExitFCM(self, iterations):
+        if self.max_diff<self.epsilon:
+            return True
+
+        if iterations <= self.max_FCM_iterations:
+            return False
+        return True
+
+    def shouldExitPCA(self, iterations):
+        if iterations <= self.max_PCA_iterations:
+            return False
+        return True
+
+    # Euclidean distance (Distance Metric).
+    def calcDistance(self, a, b):
+        result = numpy.sqrt(sum((a - b) ** 2))
+        return result
+
+    # Calculates the centroids using degree of membership and fuzziness.
+    def calculate_centre_vector(self):
+        for cluster in range(self.k):
+            sum_numerator = 0.0
+            sum_denominator = 0.0
+            for i in range(self.s_new):
+                pow_uij= pow(self.degree_of_membership[i][cluster], self.m)
+                sum_denominator +=pow_uij
+                num= pow_uij * self.pixels[i]
+
+                sum_numerator+=num
+
+            updatedcluster_center = sum_numerator/sum_denominator
+
+            self.clusters[cluster].centroid = updatedcluster_center
+
+
+    # Updates the degree of membership for all of the data points.
+    def update_degree_of_membershipFCM(self):
+        self.max_diff = 0.0
+
+        for idx in range(self.k):
+            for i in range(self.s_new):
+                new_uij = self.get_new_value(self.pixels[i], self.clusters[idx].centroid)
+                if (i == 0):
+                    print "This is the Updatedegree centroid number:", idx, self.clusters[idx].centroid
+                diff = new_uij - self.degree_of_membership[i][idx]
+                if (diff > self.max_diff):
+                    self.max_diff = diff
+                self.degree_of_membership[i][idx] = new_uij
+        return self.max_diff
+
+    def get_new_value(self, i, j):
+        sum = 0.0
+        val = 0.0
+        p = (2 * (1.0) / (self.m - 1))  # cast to float value or else will round to nearst int
+        for k in self.clusters:
+            num = self.calcDistance(i, j)
+            denom = self.calcDistance(i, k.centroid)
+            val = num / denom
+            val = pow(val, p)
+            sum += val
+        return (1.0 / sum)
+
+    def getEta(self, idx):
+        sum_membership = 0.0
+        eta_numerator = 0.0
+        eta_k = 1.0
+
+        for i in range(self.s_new):
+            dis = pow(self.calcDistance(self.clusters[idx].centroid, self.pixels[i]), 2.0)
+
+            membership_power = pow(self.degree_of_membership[i][idx], self.m)
+
+            eta_numerator += (membership_power * dis)
+
+            sum_membership += membership_power
+
+        eta =eta_numerator / sum_membership
+        eta = eta * eta_k
+        return eta
+
+    # update the degree of membership for PCA
+    def update_degree_of_membershipPCA(self):
+        #PCA 96
+        for idx in range(self.k):
+            eta = 0.0
+            eta_k = 1.0
+
+            #get eta for particular cluster
+            eta = self.getEta(idx)
+
+            if eta > 0.0:
+                # print "******************* eta", eta
+                for i in range(self.s_new):
+                    if (i == 0):
+                        print "This is the Update degree centroid number:", idx, self.clusters[idx].centroid
+
+                    dis = pow(self.calcDistance(self.clusters[idx].centroid, self.pixels[i]), 2.0)
+
+                    factor = dis / eta
+                    factor = factor * -1.0
+
+                    updated_membership_degree = math.exp(factor)
+
+                    self.degree_of_membership[i][idx] = updated_membership_degree
+
+
+
+    def showClustering(self, name):
+        localPixels = [None] * len(self.image.getdata())
+        for idx, pixel in enumerate(self.pixels2):
+            shortest = float('Inf')
+            for cluster in self.clusters:
+                distance = self.calcDistance(cluster.centroid, pixel)
+                if distance < shortest:
+                    shortest = distance
+                    nearest = cluster
+
+            # if nearest == self.clusters[0]:
+            #     localPixels[idx]=[229,75,77]
+            # elif nearest == self.clusters[1]:
+            #     localPixels[idx] = [56,129,78]
+            # elif nearest == self.clusters[2]:
+            #     localPixels[idx] = [251,227,227]
+            localPixels[idx] = nearest.centroid
+
+        w, h = self.image.size
+        localPixels = numpy.asarray(localPixels) \
+            .astype('uint8') \
+            .reshape((h, w, 4))
+        colourMap = Image.fromarray(localPixels)
+        # colourMap.show()
+
+        plt.imsave(name, colourMap)
+
+
  
 
 class Set(object):
@@ -61,7 +317,7 @@ class Set(object):
 
                 #check the boundary of image width and height
                 if col_x < self.width and row_x<self.height and col_x>0 and row_x>0:
-                    if self.calcDistance(self.gt_image_map[col_x,row_x], self.gt_image_map[41,22]) != 0.0: 
+                    if self.calcDistance(self.gt_image_map[col_x,row_x], self.gt_image_map[157,16]) != 0.0: 
                         count_majority = count_majority+1
 
 
@@ -89,7 +345,7 @@ class Set(object):
                 #check the boundary of image width and height
                 if col_x < self.width and row_x<self.height and col_x>0 and row_x>0:
                     #if it is in majority
-                    if self.calcDistance(self.gt_image_map[col_x,row_x], self.gt_image_map[41,22]) != 0.0: 
+                    if self.calcDistance(self.gt_image_map[col_x,row_x], self.gt_image_map[157,16]) != 0.0: 
                         count_majority = count_majority+1
 
 
@@ -110,7 +366,7 @@ class Set(object):
                     #check the boundary of image width and height
                     if col_x < self.width and row_x<self.height and col_x>0 and row_x>0:
                         #if it is in majority
-                        if self.calcDistance(self.gt_image_map[col_x,row_x], self.gt_image_map[41,22]) != 0.0: 
+                        if self.calcDistance(self.gt_image_map[col_x,row_x], self.gt_image_map[157,16]) != 0.0: 
                             # add the majority set
                             self.sbmaj.append((col_x, row_x))
 
@@ -132,7 +388,7 @@ class Set(object):
                     #check the boundary of image width and height
                     if col_x < self.width and row_x<self.height and col_x>0 and row_x>0:
                         #if it is in minority
-                        if self.calcDistance(self.gt_image_map[col_x,row_x], self.gt_image_map[41,22]) == 0.0: 
+                        if self.calcDistance(self.gt_image_map[col_x,row_x], self.gt_image_map[157,16]) == 0.0: 
                             count_minority = count_minority + 1
 
             # add to simin 
@@ -150,7 +406,7 @@ class Set(object):
                         #check the boundary of image width and height
                         if col_x < self.width and row_x<self.height and col_x>0 and row_x>0:
                             #if it is minority
-                            if self.calcDistance(self.gt_image_map[col_x,row_x], self.gt_image_map[41,22]) == 0.0: 
+                            if self.calcDistance(self.gt_image_map[col_x,row_x], self.gt_image_map[157,16]) == 0.0: 
                                 self.simin.append((col_x, row_x))
                                 neightbour.append((col_x, row_x))
 
@@ -175,7 +431,7 @@ class Set(object):
             for j in range(self.height):
                
                 #match with exact white pixel
-                if self.calcDistance(self.gt_image_map[i,j], self.gt_image_map[41,22]) == 0.0: 
+                if self.calcDistance(self.gt_image_map[i,j], self.gt_image_map[157,16]) == 0.0: 
                     self.minority_pixel_loc.append((i,j))
 
                     self.findSminf(i, j)
@@ -299,8 +555,198 @@ class Weight(object):
         return self.I_w, self.S_w, self.S_p
 
 
+class clustering(object):
+    def __init__(self, N_min, smin, sminf, org_image_map):
+        self.N_min = N_min
+        self.smin = smin
+        self.sminf = sminf
+        self.org_image_map = org_image_map
+        self.L = []
+        self.which_cluster = []
+        self.C_p = 3.0
+        self.d_avg = 0
+        self.threshold = 0
+       
+    def findThresholdAvg(self):
+        d_avg = 0
+
+        for i in self.sminf:
+            tmp = []
+
+            for j in self.sminf:
+                if i == j:
+                    continue
+
+                distance = self.calcEucledianDistance(i, j)
+                tmp.append(distance)
+
+                print distance
+            self.d_avg +=min(tmp)
+        self.d_avg/=len(self.sminf)
+        self.threshold = self.d_avg * self.C_p
+
+        print self.d_avg, self.threshold
+
+    def run_cluster(self):
+        self.L = { index:[i] for index, i in enumerate(self.smin)}
+
+        print len(self.smin), len(self.L)
+
+        clusters_number = range(len(self.smin))
+
+        dis_table = [ [0 for i in clusters_number] for j in clusters_number]
+
+        for i in clusters_number:
+            for j in clusters_number:
+
+                # print L[i][0], L[j]
+                dis_table[i][j] = self.calcEucledianDistance(self.L[i][0], self.L[j][0])
+                # print "distance ", dis_table[i][j]
+
+        MAX = max(max(j) for j in dis_table)
+
+        print "max ", MAX
+
+        for i in clusters_number:
+            dis_table[i][i] = MAX
+
+        for i in self.smin:
+            MIN = min(min(j) for j in dis_table)
+
+            if MIN > self.threshold:
+                break
+            for j in clusters_number:
+              if MIN in dis_table[j]:
+                b = dis_table[j].index(MIN)
+                a = j
+                break
+            self.L[a].extend(self.L[b])
+
+            del self.L[b]
+            clusters_number.remove(b)
+            for j in clusters_number:
+                tmp = self.calcEucledianDistance(self.L[a][0], self.L[j][0])
+                dis_table[a][j] = tmp
+                dis_table[j][a] = tmp
+
+            dis_table[a][a] = MAX
+
+            for j in clusters_number:
+                dis_table[b][j] = MAX
+                dis_table[j][b] = MAX
+  
+        self.which_cluster = {}
+        for i, clu in self.L.items():
+            for j in clu:
+                self.which_cluster[j] = i
+
+
+        print len(self.L), len(self.which_cluster)
+
+
+        
+
+
+
+    def getSets(self):
+        return self.L, self.which_cluster
+
+
+    def calcEucledianDistance(self, min_loc, maj_loc):
+        #get the pixel value from the org_image_map
+        maj_pixel = self.org_image_map[maj_loc[0], maj_loc[1]]
+        min_pixel = self.org_image_map[min_loc[0], min_loc[1]]
+
+        dis1 = pow((maj_pixel[0]-min_pixel[0]),2.0)
+        dis2 = pow((maj_pixel[1]-min_pixel[1]),2.0)
+        dis3 = pow((maj_pixel[2]-min_pixel[2]),2.0)
+        dis4 = pow((maj_pixel[3]-min_pixel[3]),2.0)
+
+        sumation = dis1 + dis2 + dis3 + dis4
+        result = numpy.sqrt(sumation)
+        return result
+
+
+class WeightedSampleRandomGenerator(object):
+    def __init__(self, indices, weights):
+        self.totals = []
+        self.indices = indices
+        running_total = 0
+
+        for w in weights:
+            running_total += w
+            self.totals.append(running_total)
+
+    def next(self):
+        rnd = random.random() * self.totals[-1]
+
+        return self.indices[bisect.bisect_right(self.totals, rnd)]
+
+    def __call__(self):
+        return self.next()
+
+
+class Resampling(object):
+    def __init__(self, S_w, S_p, L, which_cluster, N, org_image_map):
+        self.S_w = S_w
+        self.S_p = S_p
+        self.L = L
+        self.which_cluster = which_cluster
+        self.N = N
+        self.org_image_map = org_image_map
+        self.X_gen = []
+
+    def resample(self):
+        self.X_gen = []
+        some_big_number = 10000000.
+        sample = WeightedSampleRandomGenerator(self.S_w.keys(), self.S_w.values())
+
+        for i in range(self.N):
+            x = sample()
+            y = random.choice( self.L[self.which_cluster[x]] )
+            # print y
+
+            alpha = random.randint(0, some_big_number) / some_big_number
+
+            dis = self.calcEucledianDistance(x, y)
+
+            factor = dis * alpha
+            s = self.get_pixel_value(x) 
+
+            print s, factor
+            s = [s[0]+factor, s[1] + factor, s[2]+factor , 255] 
+            print s
+            self.X_gen.append(s)
+
+        return self.X_gen
+
+
+
+    def get_pixel_value(self, loc):
+        return self.org_image_map[loc[0], loc[1]]
+
+
+
+
+
+            # print x
+    def calcEucledianDistance(self, min_loc, maj_loc):
+        #get the pixel value from the org_image_map
+        maj_pixel = self.org_image_map[maj_loc[0], maj_loc[1]]
+        min_pixel = self.org_image_map[min_loc[0], min_loc[1]]
+
+        dis1 = pow((maj_pixel[0]-min_pixel[0]),2.0)
+        dis2 = pow((maj_pixel[1]-min_pixel[1]),2.0)
+        dis3 = pow((maj_pixel[2]-min_pixel[2]),2.0)
+        dis4 = pow((maj_pixel[3]-min_pixel[3]),2.0)
+
+        sumation = dis1 + dis2 + dis3 + dis4
+        result = numpy.sqrt(sumation)
+        return result
+
+
 if __name__ == "__main__":
-    s= Set("T3.png", "GT_T3.png")
+    s= Set("T4.png", "GT_T4.png")
     s.openImages()
     s.seperate_classes()
     majority, minority, sminf, sbmaj, simin, N_min, org_image_map = s.getSets()
@@ -312,5 +758,24 @@ if __name__ == "__main__":
     w.generateSWSP()
     I_w, S_w, S_p = w.getSets()
 
+
     print "end of finding the weights"
     print "len of weights ", len(I_w)
+
+
+    #step 10
+
+    c = clustering(N_min, simin, sminf, org_image_map)
+    c.findThresholdAvg()
+    c.run_cluster()
+    L, which_cluster = c.getSets()
+
+
+    r = Resampling(S_w, S_p, L, which_cluster, 100000, org_image_map)
+    X_gen = r.resample()
+
+    print "generated", len(X_gen)
+
+    f = fcm(X_gen)
+    image = Image.open("T4.png")
+    f.run(image)
